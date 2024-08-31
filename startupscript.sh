784e198f6cb86f7298ad68e29a42667fa39b7c59
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-08-31 11:57:26 +0200

    Create startupscript.sh

---
+++ startupscript.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Update the package list and install necessary packages
+apt update
+apt install -y tmux build-essential pkg-config libssl-dev git curl
+
+# Clone the repository
+git clone -b benchmarker_v2.0 https://github.com/tig-foundation/tig-monorepo.git
+cd tig-monorepo
+git config --global user.email “kyranmend@gmail.com” # can be anything
+git config --global user.name “KyranMendoza1” # can be anything
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/knapheudp
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git satisfiability/sat_allocd
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/cw_heurisitic
+
+cd /app
+
+# Install rustup and cargo
+bash -c '
+curl --proto =https --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
+source $HOME/.cargo/env
+export PATH="$HOME/.cargo/bin:$PATH"
+cd /app/tig-monorepo/tig-benchmarker
+ALGOS_TO_COMPILE="satisfiability_sat_allocd vehicle_routing_cw_heuristic knapsack_knapheudp vector_search_optimax_gpu"
+cargo fix --lib -p tig-algorithms --allow-dirty
+cargo build -p tig-benchmarker --release --no-default-features --features "standalone ${ALGOS_TO_COMPILE}"
+'
+
+# Switch to the target release directory
+cd /app/tig-monorepo/target/release
+
+# Select algorithms to benchmark
+SELECTED_ALGORITHMS='{"satisfiability":"sat_allocd","vehicle_routing":"clarke_wright_super","knapsack":"knapmaxxing","vector_search":"optimax_gpu"}'
+# Calculate the number of workers (number of threads x 8)
+NUM_THREADS=$(nproc)
+NUM_WORKERS=$((NUM_THREADS))
+
+# Export the variables so they are available to the tmux session
+# Start a new tmux session
+tmux new-session -d -s TIG
+
+# Send the export commands and the command to run the benchmarker
+tmux send-keys -t TIG "export SELECTED_ALGORITHMS='$SELECTED_ALGORITHMS'" C-m
+tmux send-keys -t TIG "export NUM_WORKERS='$NUM_WORKERS'" C-m
+tmux send-keys -t TIG "./tig-benchmarker 0x5de35f527176887b1b42a2703ba4d64e62a48de4 3de214b978b22a7b9c0957ccfc3a95a1 '$SELECTED_ALGORITHMS' --workers $NUM_WORKERS --master 37.60.232.241“ C-m
