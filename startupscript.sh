472c39ead2b9e856f64c36ebe3fb6ff5148ecae3
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-02 21:54:34 +0200

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -8,9 +8,9 @@ git clone -b benchmarker_v2.0 https://github.com/tig-foundation/tig-monorepo.git
 cd tig-monorepo
 git config --global user.email "kyranmend@gmail.com" # can be anything
 git config --global user.name "KyranMendoza1" # can be anything
-git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/cw_heuristic
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/clarke_wright_super
 git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
-git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/knapheudp
+git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/knapmaxxing
 git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git satisfiability/sat_allocd
 
 cd /app
@@ -21,7 +21,7 @@ curl --proto =https --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
 source $HOME/.cargo/env
 export PATH="$HOME/.cargo/bin:$PATH"
 cd /app/tig-monorepo/tig-benchmarker
-ALGOS_TO_COMPILE="satisfiability_sat_allocd vehicle_routing_cw_heuristic knapsack_knapheudp vector_search_optimax_gpu"
+ALGOS_TO_COMPILE="satisfiability_sat_allocd vehicle_routing_clarke_wright_super knapsack_knapmaxxing vector_search_optimax_gpu"
 cargo fix --lib -p tig-algorithms --allow-dirty
 cargo build -p tig-benchmarker --release --no-default-features --features "standalone ${ALGOS_TO_COMPILE}"
 '
@@ -30,7 +30,7 @@ cargo build -p tig-benchmarker --release --no-default-features --features "stand
 cd /app/tig-monorepo/target/release
 
 # Select algorithms to benchmark
-SELECTED_ALGORITHMS='{"satisfiability":"sat_allocd","vehicle_routing":"cw_heuristic","knapsack":"knapheudp","vector_search":"optimax_gpu"}'
+SELECTED_ALGORITHMS='{"satisfiability":"sat_allocd","vehicle_routing":"clarke_wright_super","knapsack":"knapmaxxing","vector_search":"optimax_gpu"}'
 
 # Calculate the number of workers (number of threads x 8)
 NUM_THREADS=$(nproc)
