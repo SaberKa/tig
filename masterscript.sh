49b45ebe205856a6b68d6ba54838cd896f3edb3f
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-05 16:49:22 +0200

    Update masterscript.sh

--- masterscript.sh
+++ masterscript.sh
@@ -18,16 +18,16 @@ tmux new-session -d -s tig-session bash -c '
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/clarke_wright_super
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/quick_knap
-    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git satisfiability/satisfactory
+    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git satisfiability/sat_allocd
 
     # Compile the selected algorithms
-    ALGOS_TO_COMPILE="satisfiability_satisfactory vehicle_routing_clarke_wright_super knapsack_quick_knap vector_search_optimax_gpu"
+    ALGOS_TO_COMPILE="satisfiability_sat_allocd vehicle_routing_clarke_wright_super knapsack_quick_knap vector_search_optimax_gpu"
     cargo build -p tig-benchmarker --release --no-default-features --features "${ALGOS_TO_COMPILE}"
 
     # Get the number of CPU threads available
     WORKERS=$(nproc)
 
     # Run the benchmarker with the number of workers set to the number of CPU threads
-    SELECTED_ALGORITHMS='"'"'{"satisfiability":"satisfactory","vehicle_routing":"clarke_wright_super","knapsack":"quick_knap","vector_search":"optimax_gpu"}'"'"'
+    SELECTED_ALGORITHMS='"'"'{"satisfiability":"sat_allocd","vehicle_routing":"clarke_wright_super","knapsack":"quick_knap","vector_search":"optimax_gpu"}'"'"'
     ./target/release/tig-benchmarker 0x5de35f527176887b1b42a2703ba4d64e62a48de4 3de214b978b22a7b9c0957ccfc3a95a1 $SELECTED_ALGORITHMS --workers 0 --duration 20000
 '
