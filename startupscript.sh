e33073ec40eb437b5fc4041015cbfaf517579c7b
SaberKa <47255503+SaberKa@users.noreply.github.com>
2024-09-07 21:06:29 +0200

    Update startupscript.sh

--- startupscript.sh
+++ startupscript.sh
@@ -13,8 +13,8 @@ tmux new-session -d -s tig-session bash -c '
     # Clone and set up the repository
     git clone -b benchmarker_v2.0 https://github.com/tig-foundation/tig-monorepo.git
     cd tig-monorepo
-    git config --global user.email "kyranmend@gmail.com"
-    git config --global user.name "KyranMendoza1"
+    git config --global user.email "katrou.saber@gmail.com"
+    git config --global user.name "SaberKa"
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/clarke_wright_super
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/quick_knap
@@ -29,5 +29,5 @@ tmux new-session -d -s tig-session bash -c '
 
     # Run the benchmarker with the number of workers set to the number of CPU threads
     SELECTED_ALGORITHMS='"'"'{"satisfiability":"sat_optima","vehicle_routing":"clarke_wright_super","knapsack":"quick_knap","vector_search":"optimax_gpu"}'"'"'
-    ./target/release/tig-benchmarker 0x5de35f527176887b1b42a2703ba4d64e62a48de4 3de214b978b22a7b9c0957ccfc3a95a1 $SELECTED_ALGORITHMS --workers $WORKERS --master 37.60.232.241
+    ./target/release/tig-benchmarker 0x5de35f527176887b1b42a2703ba4d64e62a48de4 3de214b978b22a7b9c0957ccfc3a95a1 $SELECTED_ALGORITHMS --workers $WORKERS --master 192.168.196.199
 '
