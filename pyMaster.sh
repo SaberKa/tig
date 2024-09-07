0b0d4caf6f5f8ecd5b46631f6bddb6e15d296fd1
SaberKa <47255503+SaberKa@users.noreply.github.com>
2024-09-07 21:05:03 +0200

    Update pyMaster.sh

--- pyMaster.sh
+++ pyMaster.sh
@@ -13,8 +13,8 @@ tmux new-session -d -s tig-session bash -c '
     # Clone and set up the repository
     git clone -b benchmarker_v2.0 https://github.com/tig-foundation/tig-monorepo.git
     cd tig-monorepo
-    git config --global user.email "kyranmend@gmail.com"
-    git config --global user.name "KyranMendoza1"
+    git config --global user.email "katrou.saber@gmail.com"
+    git config --global user.name "SaberKa"
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/clarke_wright_super
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
     git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/quick_knap
@@ -26,6 +26,6 @@ tmux new-session -d -s tig-session bash -c '
     #run Master
     cd tig-benchmarker
     pip3 install -r requirements.txt
-    wget -O /root/tig-monorepo/tig-benchmarker/master/config.py https://raw.githubusercontent.com/KyranMendoza1/tig/main/config.py
+    wget -O /home/saber/tig-monorepo/tig-benchmarker/master/config.py https://raw.githubusercontent.com/SaberKa/tig/main/config.py
     python3 main.py
 '
