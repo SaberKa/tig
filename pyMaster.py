423f8e31b2aa12e8f76340f58010723c5afe6a3e
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-07 14:08:59 +0200

    Create pyMaster.py

---
+++ pyMaster.py
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Update the package list and install necessary packages
+apt update
+apt install -y tmux build-essential pkg-config libssl-dev git curl
+
+# Start a new tmux session named 'tig-session' and run the commands inside it
+tmux new-session -d -s tig-session bash -c '
+    # Install Rust
+    curl --proto =https --tlsv1.3 https://sh.rustup.rs -sSf | sh -s -- -y
+    source $HOME/.cargo/env
+    export PATH="$HOME/.cargo/bin:$PATH"
+
+    # Clone and set up the repository
+    git clone -b benchmarker_v2.0 https://github.com/tig-foundation/tig-monorepo.git
+    cd tig-monorepo
+    git config --global user.email "kyranmend@gmail.com"
+    git config --global user.name "KyranMendoza1"
+    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vehicle_routing/clarke_wright_super
+    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git vector_search/optimax_gpu
+    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git knapsack/quick_knap
+    git pull --no-edit --no-rebase https://github.com/tig-foundation/tig-monorepo.git satisfiability/sat_allocd
+
+    #build tig-worker
+    cargo build -p tig-worker --release
+
+    #run Master
+    cd tig-benchmarker
+    pip3 install -r requirements.txt
+    wget -O /root/tig-monorepo/tig-benchmarker/master/config.py https://raw.githubusercontent.com/KyranMendoza1/tig/main/config.py
+    python3 main.py
+'
