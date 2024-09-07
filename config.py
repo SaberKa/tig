abdf201b246c93ccd0f97339eb4d221e6f43222a
KyranMendoza1 <42067499+KyranMendoza1@users.noreply.github.com>
2024-09-07 14:41:22 +0200

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ JOBS = dict(
         sat_allocd=dict(
             benchmark_duration=10000, # amount of time to run the benchmark in milliseconds
             wait_slave_duration=5000, # amount of time to wait for slaves to post solutions before submitting
-            num_jobs=2, # number of jobs to create. each job will sample its own difficulty
+            num_jobs=1, # number of jobs to create. each job will sample its own difficulty
             weight=1.0, # weight of jobs for this algorithm. more weight = more likely to be picked
         )
     ),
@@ -23,7 +23,7 @@ JOBS = dict(
         clarke_wright_super=dict(
             benchmark_duration=10000,
             wait_slave_duration=5000,
-            num_jobs=2,
+            num_jobs=1,
             weight=1.0,
         )
     ),
@@ -31,7 +31,7 @@ JOBS = dict(
         quick_knap=dict(
             benchmark_duration=10000,
             wait_slave_duration=5000,
-            num_jobs=2,
+            num_jobs=1,
             weight=1.0,
         )
     ),
@@ -39,7 +39,7 @@ JOBS = dict(
         optimax_gpu=dict(
             benchmark_duration=30000, # recommend a high duration
             wait_slave_duration=30000, # recommend a high duration
-            num_jobs=2,
+            num_jobs=1,
             weight=1.0,
         )
     ),
